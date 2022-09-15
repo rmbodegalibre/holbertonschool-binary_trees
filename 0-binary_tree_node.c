4f0ac86c809e72cb45f1fbe0f2617eaed97479ad
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-15 17:33:27 -0500

    0-binary_tree_node.c 03

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,9 +13,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
-	node->parent = parent;
-	node->n = value;
-	node->left = NULL;
-	node->right = NULL;
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
 	return (new);
 }
