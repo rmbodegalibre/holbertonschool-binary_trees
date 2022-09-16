50e99e857dee5978283ad9bbf67e6c66a9f1d4ab
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-16 09:48:23 -0500

    9-binary_tree_height.c 02

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,8 +15,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	size_t left, right;
 
-	left = binary_tree_height(tree->left) + 1;
-	right = binary_tree_height(tree->right) + 1;
+
+	left = (tree->left) ? 1 + binary_tree_height(tree->left) : 0;
+	right = (tree->right) ? 1 + binary_tree_height(tree->right) : 0;
 
 	if (left > right)
 	{
