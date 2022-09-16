3527053f6ef683a7cda7b90f73357382642cdf49
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-15 23:52:19 -0500

    1-binary_tree_insert_left.c 02

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,20 +14,21 @@
  *              takes its place and the old left-child is set as
  *              the left-child of the new node.
  */
+ 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
 	if (parent == NULL)
-    {
+	{
 		return (NULL);
-    }
+	}
 
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
-    {
+	{
 		return (NULL);
-    }
+	}
 
 	if (parent->left != NULL)
 	{
