ea87199c05311d46b0cd932c49158ab8bae06677
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-16 09:52:08 -0500

    9-binary_tree_height.c 04

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,10 +6,11 @@
  * Return: height of tree or 0 if NULL
  */
 
-size_t left, right;
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+
+	size_t left, right;
+
 	if (!tree)
 	{
 		return (0);
